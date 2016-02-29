945bd78dd0787bb5e4d43d35772df189c46f7fe7
Harry Serko <harryserko@gmail.com>
2016-02-29 20:59:02 +0200

    no message

--- Currency.js
+++ Currency.js
@@ -3,32 +3,36 @@ var http = require('http');
 
 var token = '197462224:AAEDcz9mG3CM1M_dAK_XPF_Qp9wGGG7X-NA';
 var botOptions = {
-    poling: true,
+  poling: true,
 };
 
 var bot = new TelegramBot(token, botOptions);
 
 var options = {
-    host: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
-}
+  host: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+};
 
 var content = '';
 
-bot.getMe().then(function(me)
-{
-    console.log('Hello! My name is %s!', me.first_name);
-    console.log('My id is %s.', me.id);
-    console.log('And my username is @%s.', me.username);
+bot.getMe().then(function(me) {
+  console.log('Hello! My name is %s!', me.first_name);
+  console.log('My id is %s.', me.id);
+  console.log('And my username is @%s.', me.username);
 });
- 
-bot.on('text', function(msg)
-{
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUser = msg.from.username;
- 
-    if (messageText.indexOf('/currency') === 0) {
-        updateGlobalCurrencyList(messageChatId);
-    }
+
+bot.on('text', function(msg) {
+  var messageChatId = msg.chat.id;
+  var messageText = msg.text;
+  var messageDate = msg.date;
+  var messageUser = msg.from.username;
+
+  if (messageText === '/hello') {
+    sendMessageByBot(messageChatId, 'Hello!');
+  }
+
+  console.log(msg);
 });
+
+function sendMessageByBot(aChatId, aMessage) {
+  bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}

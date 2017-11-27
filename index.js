083afe1cb10989fe62d09c0e0c7dd573013913ee
Harry Serko <serko_dio@ukr.net>
2017-11-27 17:46:22 +0200

    Express server

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-var express = require('express');
-var currency = require('./Currency');
+const TOKEN = process.env.TELEGRAM_TOKEN || '197462224:AAEDcz9mG3CM1M_dAK_XPF_Qp9wGGG7X-NA';
+const url = process.env.APP_URL || 'https://hryvnia.herokuapp.com';
+const port = process.env.PORT || 3000;
 
-var app = express();
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-app.get('/', (req, res) => {
-  res.send('ГривняБот');
-}
-);
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// Main page
+app.get('/', (req, res) => res.send('<h1 style="font-family: sans-serif;">ГривняБот</h1>'));
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('text', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
